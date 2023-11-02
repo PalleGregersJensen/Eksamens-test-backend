a897f7cd2534d306f5484fd08e58d57aedfeaec4
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-11-02 04:57:34 +0100

    Backend er sat op og kører

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import { Router } from "express";
+import { connection } from "./database.js";
+import send from "send";
+
+const router = Router();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+// Select all from Vancant_Shifts table
+app.get("/vacant_shifts", (request, response) => {
+  const query = "SELECT * FROM Vacant_shifts";
+  console.log(query);
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
+// App running on port 3000
+app.listen(port, () => {
+  console.log(`Server running on port${port}`);
+});
+
+// Make sure, that the backend is running
+app.get("/", (request, response) => {
+    console.log("Backend kører");
+    response.send("Backend kører");
+})
